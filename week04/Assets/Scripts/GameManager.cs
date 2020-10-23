12f66df58ec0388462f1e4bd085d26e2289f1fde
Buse Dagidir <buse.dagidir@ogr.iu.edu.tr>
2020-10-24 02:17:17 +0300

    Add week03 and week04

---
+++ week04/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq; //find & search
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    //progress bar için
+    private static GameManager _instance;
+    private GameState _gameState;
+    private Slider _progressBar;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public static GameManager Instance()
+    {
+        return _instance;
+    }
+
+    private void Start()
+    {
+        PrepareGame();
+    }
+
+    private void PrepareGame()
+    {
+        _gameState = new GameState();
+        _gameState.totalCheckPoint = 5;
+    }
+
+    //oyun sonu 
+   // public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //baslangıc ekranı icin
+    
+}
+
